8496372bcbf4a7f5a77ca1a5c9cccf9df68f1aaa
John Slota <jslota17@comcast.net>
2020-07-25 01:14:34 -0500

    Created seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', false);
