30fe93b96036868efa1269bbf7b1eb1e490c28a9
John Slota <jslota17@comcast.net>
2020-07-25 01:26:39 -0500

    Created connection.js file

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS burger_db;
 
-CREATE DATABASE burgers_db;
+CREATE DATABASE burger_db;
 
-USE burgers_db;
+USE burger_db;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_names VARCHAR(255),
-    devoured BOOLEAN,
-    primary key (id)
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
